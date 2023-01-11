3997fe5cac02bb9a4e54b22e76b5469c70aa4f86
김이현 <yryryo@gmail.com>
2023-01-11 18:22:38 +0900

    uiux작성중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mode">
+        모드 
+      </div>
+      <div className="scan">
+        스캔
+      </div>
+      <div className="recipe">
+        레시피
+      </div>
+      <div className="prepare">
+        dd
+      </div>
     </div>
   );
 }
